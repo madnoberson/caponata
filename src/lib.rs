271689b180d62f939d417ed0b600f06d470b6b06
madnoberson <baseddepartmentzx77@gmail.com>
2025-09-07 19:09:47 +0300

    Rename to caponata

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #[cfg(feature = "small-spinner-widget")]
 #[doc(inline)]
-pub use ratatui_small_spinner as small_spinner;
+pub use caponata_small_spinner as small_spinner;
 
 #[cfg(feature = "button-widget")]
 #[doc(inline)]
-pub use ratatui_button as button;
+pub use caponata_button as button;
 
 #[cfg(feature = "small-text-widget")]
 #[doc(inline)]
-pub use ratatui_small_text as small_text;
+pub use caponata_small_text as small_text;
